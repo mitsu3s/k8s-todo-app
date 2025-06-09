ab446b596ebcc61ee9a3e89b9973b52a6a4abaf6
mitsu3s <mitsu3s.dev@gmail.com>
2025-06-09 10:46:17 +0900

    Add docker settings

---
+++ .docker/db/init.d/2-sample.sql
@@ -0,0 +1,6 @@
+INSERT INTO 
+  todos (text, done) 
+VALUES
+  ('todo-1', false),
+  ('todo-2', true),
+  ('todo-3', false);
--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port: Number(process.env.PGPORT),
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: Number(process.env.DATABASE_PORT),
 });
